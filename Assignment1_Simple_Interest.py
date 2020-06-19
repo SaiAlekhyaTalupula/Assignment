f74b8eee13e3f0eb7309753b7bcc8583e4d274d4
TSaiAlekhya <67145656+alekhya1838@users.noreply.github.com>
2020-06-19 19:49:33 +0530

    Add files via upload

---
+++ Assignment1_Simple_Interest.py
@@ -0,0 +1,4 @@
+p=int(input("Enter principle amount:"))
+t=int(input("Enter the time duration(In years):"))
+r=int(input("Enter the interest rate:"))
+print("Simple interest is:",((p*t*r)/100),"ruppees")
